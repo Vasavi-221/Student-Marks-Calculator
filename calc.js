16d8587f6ca2214ffbdee4d511ba0d8594b0ab38
Vasavi-221 <vasaviveeranki297@gmail.com>
2025-07-07 22:35:07 +0530

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,17 @@
+function calculate(){
+  let name = document.getElementById("name").value;
+  let s1 = parseFloat(document.getElementbyId("sub1").value);
+  let s2 = parseFloat(document.getElementbyId("sub2").value);
+  let s3 = parseFloat(document.getElementbyId("sub3").value);
+  let total = s1+s2+s3;
+  let avg = total/3;
+  let grade;
+  if(avg>=75) grade="A";
+  else if(avg>=60) grade="B";
+  else if(avg>=45) grade="C";
+  else grade = "Fail";
+  document.getElementById("result").innerHTML=
+    "<p>" +name+ ",Total:" +total+ "</p>" +
+    "<p> Average:" +avg.toFixed(2)+ "</p>" +
+    "<p> Grade:" +grade+ "</p>";
+  
